4bb0640e2516b5ed22048be925567622521f42bf
Joaquin Ollo <joaquinollo@gmail.com>
2017-10-27 19:57:52 -0300

    Clases

---
+++ Ejercicio-3-cuentas-bancarias-/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Persona
+{
+    class Titular
+    {
+         public long DNI {get;set}
+         public string NombreCompleto {get;set}
+         public string mail {get;set}
+         public long Teléfono {get;set}
+         public DateTime FechaDeNacimiento {get;set}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
